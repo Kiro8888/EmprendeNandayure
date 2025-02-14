856efbb6fc1fa8398fc6456209fe4056ab84a1ce
FiorellaBonilla <fiorella.bonilla.gonzalez@est.una.ac.cr>
2025-02-14 12:08:55 -0600

    tilde

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -20,7 +20,7 @@
                 </div>
                   {{-- Segundo campo --}}
                 <div class="form-group">
-                    <label for="pdt_description" class="form-label">Descripcion</label>
+                    <label for="pdt_description" class="form-label">Descripción</label>
                     <input type="text" class="form-control" name="pdt_description" id="pdt_description">
                     @error('pdt_description')
                     <p class="text-danger">{{$message}}</p>
